e1691b279cf87fc7086d27ba0ac7af12d6e64d0f
jakub.coufal <jakub.coufal@wandera.com>
2018-09-06 23:45:38 +0200

    Update solution

--- step2/src/com/wandera/workshop/Application.kt
+++ step2/src/com/wandera/workshop/Application.kt
@@ -8,23 +8,28 @@ import com.wandera.workshop.service.UserService
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
 import io.ktor.html.respondHtml
 import io.ktor.jackson.jackson
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.server.netty.DevelopmentEngine
 import kotlinx.html.body
 import kotlinx.html.h1
 import kotlinx.html.head
 import kotlinx.html.title
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+fun main(args: Array<String>): Unit = DevelopmentEngine.main(args)
 
 private val userService = UserService()
 private val kweetService = KweetService()
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
